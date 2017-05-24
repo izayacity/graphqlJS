a9d326e7ac191b6fc2d67dfb6284f6ca75544971
Archer <archer@eduroam-bowers-128-114-155-167.ucsc.edu>
2017-05-23 19:52:12 -0700

    Added GitHub api for query as UserInfoType

--- app.js
+++ app.js
@@ -18,39 +18,74 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+var axios = require('axios');
 
-const query = new GraphQLObjectType({
-    name: "Query",
-    description: "First",
+// https://api.github.com/users/apaquino
+const UserInfoType = new GraphQLObjectType({
+    name: "UserInfo",
+    description: "Basic information on a GitHub user",
     fields: () => ({
-        hello: {
+        "login":                { type: GraphQLString },
+        "id":                   { type: GraphQLInt },
+        "avatar_url":           { type: GraphQLString },
+        "site_admin":           { type: GraphQLBoolean },
+        "url":                  { type: GraphQLString },
+        "html_url":             { type: GraphQLString },
+        "followers_url":        { type: GraphQLString },
+        "gists_url":            { type: GraphQLString },
+        "starred_url":          { type: GraphQLString },
+        "subscriptions_url":    { type: GraphQLString },
+        "organizations_url":    { type: GraphQLString },
+        "repos_url":            { type: GraphQLString },
+        "events_url":           { type: GraphQLString },
+        "received_events_url":  { type: GraphQLString },
+        "type":                 { type: GraphQLString },
+        "site_admin":           { type: GraphQLBoolean },
+        "name":                 { type: GraphQLString },
+        "company":              { type: GraphQLString },
+        "blog":                 { type: GraphQLString },
+        "location":             { type: GraphQLString },
+        "email":                { type: GraphQLString },
+        "hireable":             { type: GraphQLString },
+        "bio":                  { type: GraphQLString },
+        "public_repos":         { type: GraphQLInt },
+        "public_gists":         { type: GraphQLInt },
+        "followers":            { type: GraphQLInt },
+        "following":            { type: GraphQLInt },
+        "created_at":           { type: GraphQLString },
+        "updated_at":           { type: GraphQLString },
+        "following_url": {
             type: GraphQLString,
-            description: "Accepts a name so you can be nice and say hi",
-            args: {
-                name: {
-                    type: new GraphQLNonNull(GraphQLString),
-                    description: "Name you want to say hi to",
-                }
-            },
-            resolve: (_,args) => {
-                return `Hello, ${args.name}!!!`;
-            }
-        },
-        luckyNumber: {
-            type: GraphQLInt,
-            description: "A lucky number",
-            resolve: () => {
-                return 888;
+            resolve: (obj) => {
+                const brackIndex = obj.following_url.indexOf("{");
+                return obj.following_url.slice(0, brackIndex);
             }
         }
     })
 });
-/*
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);*/
+
+const query = new GraphQLObjectType({
+    name: "Query",
+    description: "First GraphQL Server Config — Yay!",
+    fields: () => ({
+        gitHubUser: {
+            type: UserInfoType,
+            description: "GitHub user API data with enhanced and additional data",
+            args: {
+                username: {
+                    type: new GraphQLNonNull(GraphQLString),
+                    description: "The GitHub user login you want information on",
+                },
+            },
+            resolve: (_,{username}) => {
+                const url = `https://api.github.com/users/${username}`;
+                return axios.get(url).then(function(response) {
+                    return response.data;
+                });
+            }
+        },
+    })
+});
 
 const schema = new GraphQLSchema({
     query
